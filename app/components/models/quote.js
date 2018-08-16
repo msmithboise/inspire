895d74a81083bf2cfaf58199d8e591576817a18b
Michael Smith <msmithboise@gmail.com>
2018-08-16 16:59:48 -0600

    push quote model layout

---
+++ app/components/models/quote.js
@@ -0,0 +1,10 @@
+export default class Quote {
+    constructor(data) {
+
+        this.id = _id
+        this.quote = quote
+        this.author = author
+        this.permalink = permalink
+
+    }
+}
--- app/components/quote/quote-service.js
+++ app/components/quote/quote-service.js
@@ -3,6 +3,9 @@ let url2 = 'http://quotesondesign.com/api/3.0/api-3.0.json';
 let apiUrl = url + encodeURIComponent(url2);
 //Do Not Edit above we have to go through the bcw-getter to access this api
 
+import Quote from '../../components/models/quote.js'
+
+let quote = new Quote()
 
 const quoteApi = axios.create({
 	baseURL: apiUrl,
@@ -12,9 +15,9 @@ const quoteApi = axios.create({
 
 export default class QuoteService {
 	getQuote(callWhenDone) {
-		console.log('looking for some good quotes')
 		quoteApi().then((res) => {
 			callWhenDone(res.data)
+			console.log(res.data)
 		})
 	}
 }
